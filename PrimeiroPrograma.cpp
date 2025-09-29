ef40a186c11334f131fe2e96d5ce3d5b59cc59cd
uDxkr <158834969+uDxkr@users.noreply.github.com>
2025-09-29 11:47:08 +0100

    Primeiro Commit
    Programa que manda uma string para o ecra

---
+++ PrimeiroPrograma.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Sua mae e tao burra que quando o computador pega virus ela coloca remedio no buraco usb";
+
+    return 0;
+}
