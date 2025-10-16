690487c8dcb5661c4dad25459243a92f4a950ebc
uDxkr <158834969+uDxkr@users.noreply.github.com>
2025-10-16 16:37:12 +0100

    Create CalculosChatos.cpp

---
+++ CalculosChatos.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <locale>
+#include <string>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	int num[10];
+	int pos[10];
+	int times = 0, soma = 0;
+	char neg, talvez;
+	for (int i = 0; i < 10; i++) {
+		cout << "Introduz numeros: ";
+
+		cin >> num[i];
+		if (num[i] < 0) {
+			pos[times++] = num[i];
+		}
+	}
+	for (int i = 0; i < times; i++) {
+		soma += pos[i];
+	}
+
+	cout << "Queres somar os numeros? (s/n) ";
+	cin >> neg;
+	if(neg == 's') {
+		cout << "A soma é: " << soma << "!";
+	} else {
+		cout << "Os números negativos são: " << times;
+	} 
+
+	return 0;
+}
