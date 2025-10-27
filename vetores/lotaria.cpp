39a5d0e6e5dee77388dda7ff15f9679bc996a160
uDxkr <158834969+uDxkr@users.noreply.github.com>
2025-10-27 12:41:50 +0000

    Create lotaria.cpp

---
+++ vetores/lotaria.cpp
@@ -0,0 +1,69 @@
+    #include <iostream>
+    #include <locale>
+    #include <string>
+    #include <time.h>
+    #include <cstdlib>
+    #include <algorithm>
+
+    using namespace std;
+
+    int main() {
+        setlocale(LC_ALL, "Portuguese");
+
+        int lotaria[5];
+        srand(time(0));
+        int num, certo = 0;
+        for (int i = 0; i < 5; i++) {
+
+            lotaria[i] = (rand() % 49) + 1;
+            cout << "DEBUG NUMEROS\n" << lotaria[i] << "\n";
+
+            cout << "diga numeros de 1 a 50: ";
+            cin >> num;
+                if (num > 50 || num < 1) {
+
+                    cout << "Numero invalido! tente dnv\n";
+                    i--;
+
+                }
+                else {
+                    if (num == lotaria[i]) {
+                        certo++;
+                    }
+                }
+        }
+
+        cout << "Numeros certos = " << certo << "\n";
+
+
+        for (int i = 0; i < 5; i++) {
+            int tst = i;
+            for (int j = i + 1; j < 5; j++) {
+                if (lotaria[j] < lotaria[tst]) {
+                    tst = j;
+                }
+                if (tst != i) {
+                    int teste = lotaria[i];
+                    lotaria[i] = lotaria[tst];
+                    lotaria[tst] = teste;
+                }
+            }
+        }
+
+        //  sort(lotaria, lotaria + 5);
+        cout << "Os numeros da lotaria eram: \n";
+        for (int i = 0; i < 5; i++) {
+
+            cout << lotaria[i] << "\n";
+        }
+        if (certo == 5) {
+            cout << "parabens acertaste\n";
+        }
+        else {
+            cout << "erraste tudo mano\n ";
+        }
+        cin.ignore();
+        cin.get();
+
+        return 0;
+    }
