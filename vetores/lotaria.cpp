5eb796df121e60402abb64039db5d53f07fad213
uDxkr <158834969+uDxkr@users.noreply.github.com>
2025-10-28 14:42:20 +0000

    Update lotaria.cpp

--- vetores/lotaria.cpp
+++ vetores/lotaria.cpp
@@ -1,69 +1,76 @@
-    #include <iostream>
-    #include <locale>
-    #include <string>
-    #include <time.h>
-    #include <cstdlib>
-    #include <algorithm>
+#include <iostream>
+#include <locale>
+#include <string>
+#include <time.h>
+#include <cstdlib>
+#include <algorithm>
 
-    using namespace std;
+using namespace std;
 
-    int main() {
-        setlocale(LC_ALL, "Portuguese");
+int main() {
+    setlocale(LC_ALL, "Portuguese");
 
-        int lotaria[5];
-        srand(time(0));
-        int num, certo = 0;
-        for (int i = 0; i < 5; i++) {
-
-            lotaria[i] = (rand() % 49) + 1;
-            cout << "DEBUG NUMEROS\n" << lotaria[i] << "\n";
-
-            cout << "diga numeros de 1 a 50: ";
-            cin >> num;
-                if (num > 50 || num < 1) {
-
-                    cout << "Numero invalido! tente dnv\n";
-                    i--;
-
-                }
-                else {
-                    if (num == lotaria[i]) {
-                        certo++;
-                    }
-                }
-        }
-
-        cout << "Numeros certos = " << certo << "\n";
-
-
-        for (int i = 0; i < 5; i++) {
-            int tst = i;
-            for (int j = i + 1; j < 5; j++) {
-                if (lotaria[j] < lotaria[tst]) {
-                    tst = j;
-                }
-                if (tst != i) {
-                    int teste = lotaria[i];
-                    lotaria[i] = lotaria[tst];
-                    lotaria[tst] = teste;
-                }
+    int lotaria[5];
+    srand(time(0));
+    int num, certo = 0, newnumber, rep;
+    for (int i = 0; i < 5; i++) {
+        do { 
+                rep = false;
+        newnumber = (rand() % 49) + 1;
+        for(int j = 0; j < i;j++) {
+            if (lotaria[j] == newnumber) {
+                rep = true;
+                break;
             }
-        }
+          }           
+        } while (rep);
+        lotaria[i] = (rand() % 49) + 1;
+        cout << "DEBUG NUMEROS\n" << lotaria[i] << "\n";
 
-        //  sort(lotaria, lotaria + 5);
-        cout << "Os numeros da lotaria eram: \n";
-        for (int i = 0; i < 5; i++) {
+        cout << "diga numeros de 1 a 50: ";
+        cin >> num;
+        if (num > 50) {
 
-            cout << lotaria[i] << "\n";
-        }
-        if (certo == 5) {
-            cout << "parabens acertaste\n";
+            cout << "Numero invalido! tente dnv\n";
+            i--;
+
         }
         else {
-            cout << "erraste tudo mano\n ";
+            if (num == lotaria[i]) {
+                certo++;
+            }
         }
-        cin.ignore();
-        cin.get();
-
-        return 0;
     }
+
+    cout << "Numeros certos = " << certo << "\n";
+
+    for (int i = 0; i < 5; i++) {
+        int tst = i;
+        for (int j = i + 1; j < 5; j++) {
+            if (lotaria[j] < lotaria[tst]) {
+                tst = j;
+            }
+            if (tst != i) {
+                int teste = lotaria[i];
+                lotaria[i] = lotaria[tst];
+                lotaria[tst] = teste;
+            }
+        }
+    }
+
+    //  sort(lotaria, lotaria + 5);
+    cout << "Os numeros da lotaria eram: \n";
+    for (int i = 0; i < 5; i++) {
+
+        cout << lotaria[i] << "\n";
+    }
+    if (certo == 5) {
+        cout << "parabens acertaste\n";
+    } else {
+        cout << "erraste tudo mano\n ";
+    }
+    cin.ignore();
+    cin.get();
+
+    return 0;
+}
